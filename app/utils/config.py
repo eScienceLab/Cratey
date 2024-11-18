909eb5f629c63a9aaeb480f0aac0930c626a1c9f
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2024-11-18 15:49:33 +0000

    feat: Config file for Celery, MinIO and Flask
    Addresses: #19
    Adds boilerplate config for the API including key, celery broker URL and MinIO, set in an environment file. Also begins setting config for Celery within Flask, so tasks use shared context.

---
+++ app/utils/config.py
@@ -0,0 +1,50 @@
+import os
+
+from celery import Celery
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
+
+    # Celery configuration:
+    CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
+    # MinIO configuration:
+    MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+    MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+    MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+    MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME", "bucket-name")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ENV = "development"
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = "production"
+
+
+def make_celery(app=None):
+    celery = Celery(
+        app.import_name if app else __name__,
+        broker=os.getenv("CELERY_BROKER_URL"),
+        backend=os.getenv("CELERY_RESULT_BACKEND"),
+    )
+
+    if app:
+        celery.conf.update(app.config)
+
+        # Tasks should use the Flask app context:
+        TaskBase = celery.Task
+
+        class ContextTask(TaskBase):
+            def __call__(self, *args, **kwargs):
+                with app.app_context():
+                    return TaskBase.__call__(self, *args, **kwargs)
+
+        celery.Task = ContextTask
+
+    return celery
