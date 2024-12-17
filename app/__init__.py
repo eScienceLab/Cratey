e9a0cbd67fd07280a0879543a7b477a9ef12f990
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2024-12-17 14:38:45 +0000

    create_app config supports celery, with dev and prod environments
    Addresses #23
    Now works with Celery

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,24 @@ import os
 from flask import Flask
 
 from app.ro_crates.routes import ro_crates_bp
-from app.utils.config import DevelopmentConfig, ProductionConfig
+from app.utils.config import DevelopmentConfig, ProductionConfig, make_celery
 
 
 def create_app():
     app = Flask(__name__)
-
     app.register_blueprint(ro_crates_bp, url_prefix="/ro_crates")
 
+    # Load configuration:
     if os.getenv("FLASK_ENV") == "production":
         app.config.from_object(ProductionConfig)
     else:
         # Development environment:
+        app.debug = True
         print("URL Map:")
         for rule in app.url_map.iter_rules():
             print(rule)
         app.config.from_object(DevelopmentConfig)
 
-    return app
+    celery = make_celery(app)
+
+    return app, celery
