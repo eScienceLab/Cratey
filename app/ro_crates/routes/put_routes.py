77fb1f6da013f9bf7626057a7b15d4fc630dd6fa
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2025-01-03 13:31:25 +0000

    refactor: Remove unused files
    - Addresses #24
    - Removes API stubs that aren't implemented
    - Removes unused stubs

--- app/ro_crates/routes/put_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-ro_crates_bp = Blueprint('ro_crates', __name__)
-
-
-@ro_crates_bp.route('/ro-crates/<id>', methods=['PUT'])
-def put_ro_crate(id):
-    data = request.get_json()
-    return jsonify({'message': f'Crate {id} updated'}), 200
