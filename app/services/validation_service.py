b69d742c8222587850c4afe263f30fa5a14b49f8
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2025-01-06 09:43:27 +0000

    feat: Queue RO-Crate for validation with Celery
    - Addresses #25, #3
    - Sets up basic logging
    - Sets up basic error handling
    - Queues an RO crate for validation with process_validation_task_by_id.delay()

---
+++ app/services/validation_service.py
@@ -0,0 +1,36 @@
+"""Service methods to queue RO-Crates for validation using the CRS4 validator and Celery."""
+
+# Author: Alexander Hambley
+# License: BSD 3-Clause
+
+import logging
+
+from flask import jsonify, Response
+
+from app.tasks.validation_tasks import process_validation_task_by_id
+
+
+def queue_ro_crate_validation_task(
+    crate_id, profile_name=None, webhook_url=None
+) -> tuple[Response, int]:
+    """
+    Queues an RO-Crate for validation with Celery.
+
+    :param crate_id: The ID of the RO-Crate to validate.
+    :param profile_name: The profile to validate against.
+    :param webhook_url: The URL to POST the validation results to.
+    :return: A tuple containing a JSON response and an HTTP status code.
+    :raises: Exception: If an error occurs whilst queueing the task.
+    """
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"Processing: {crate_id}, {profile_name}, {webhook_url}")
+
+    if not crate_id:
+        return jsonify({"error": "Missing required parameter: crate_id"}), 400
+
+    try:
+        process_validation_task_by_id.delay(crate_id, profile_name, webhook_url)
+        return jsonify({"message": "Validation in progress"}), 202
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
